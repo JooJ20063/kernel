3d80edd737b2289de13791890d880c55381703a5
JooJ20063 <jose2006.marcondes@gmail.com>
2026-02-25 12:22:10 -0300

    Improve robustness docs and PS/2 keyboard IRQ safety check

--- include/arch/x86/irq.h
+++ include/arch/x86/irq.h
@@ -1,5 +1,11 @@
 #pragma once
+
+#include <stdint.h>
 #include <arch/x86/regs.h>
 
-void irq_handler_c(registers_t *regs);
+void irq_init(uint32_t timer_hz, uint32_t scheduler_quantum_ticks);
+uint32_t irq_timer_ticks(void);
+uint32_t irq_timer_seconds(void);
+uint32_t irq_timer_hz(void);
 
+void irq_handler_c(registers_t *regs);
---
+++ include/kernel/shell.h
@@ -0,0 +1,4 @@
+#pragma once
+
+void shell_init(void);
+void shell_on_key(char c);
